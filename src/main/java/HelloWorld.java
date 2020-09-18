9451e9673ad52c3b9db0fc7e5a20cd1b4faaa80e
Gowtham0786 <54531133+Gowtham0786@users.noreply.github.com>
2020-09-18 22:03:18 +0530

    Updated Helloworld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,5 +10,10 @@ public class HelloWorld implements SparkApplication {
 	@Override
 	public void init() {
 		get("/hello", (req, res) -> "Hello World");
+		System.out.println("Hello World Program00110");
+    		System.out.println("COMMITED FROM GITHUB Test Line of Codes ###");
+    		System.out.println("Push Event for Jenkins Build Trigger 001");  // Sample Commit
+    		System.out.println("### End of the Program ###12345");
+    		System.out.println("### End of the Program ###1234+++++++++");
 	}
 }
